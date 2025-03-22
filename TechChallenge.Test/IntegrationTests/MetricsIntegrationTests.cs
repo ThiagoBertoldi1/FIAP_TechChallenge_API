969a27ea6144a4cd93bf04fa7c36bedd354d98e0
Thiago <bertoldi.thiago1@gmail.com>
2025-03-22 17:27:46 -0300

    Adicionando Testes de métricas

---
+++ TechChallenge.Test/IntegrationTests/MetricsIntegrationTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using System.Net;
+
+namespace TechChallenge.Test.IntegrationTests;
+public class MetricsIntegrationTests
+{
+    private readonly HttpClient _client = new() { BaseAddress = new Uri("http://localhost:7165") };
+
+    [Fact]
+    public async Task MetricsEndpoint_ShouldReturn_Success_And_Contain_Metric_Api_Request_Count()
+    {
+        var response = await _client.GetAsync("/metrics");
+        var content = await response.Content.ReadAsStringAsync();
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        content.Should().Contain("api_request_count");
+    }
+
+    [Fact]
+    public async Task MetricsEndpoint_ShouldReturn_Success_And_Contain_Metric_CPU_Usage_Percentage()
+    {
+        var response = await _client.GetAsync("/metrics");
+        var content = await response.Content.ReadAsStringAsync();
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        content.Should().Contain("cpu_usage_percentage");
+    }
+
+    [Fact]
+    public async Task MetricsEndpoint_ShouldReturn_Success_And_Contain_Metric_Api_Memory_Usage_Bytes()
+    {
+        var response = await _client.GetAsync("/metrics");
+        var content = await response.Content.ReadAsStringAsync();
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        content.Should().Contain("api_memory_usage_bytes");
+    }
+
+    [Fact]
+    public async Task MetricsEndpoint_ShouldReturn_Success_And_Contain_Metric_Microsoft_Aspnetcore_Hosting_Http_Server_Request_Duration_Bucket()
+    {
+        var response = await _client.GetAsync("/metrics");
+        var content = await response.Content.ReadAsStringAsync();
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        content.Should().Contain("microsoft_aspnetcore_hosting_http_server_request_duration_bucket");
+    }
+}
