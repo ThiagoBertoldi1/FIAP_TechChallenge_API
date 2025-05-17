305918e82ab3e54ab3cc68f6a7f03289e68499c1
Thiago <bertoldi.thiago1@gmail.com>
2025-05-17 15:48:33 -0300

    fix: remove write methods

--- TechChallenge.Test/ContactTests/ContactTest.cs
+++ TechChallenge.Test/ContactTests/ContactTest.cs
@@ -1,14 +1,10 @@
-﻿using Moq;
-using TechChallenge.Domain.Entities;
-using TechChallenge.Domain.Interface;
+﻿using TechChallenge.Domain.Entities;
 using TechChallenge.Domain.Validations;
 using TechChallenge.Infra.Exceptions;
 
 namespace TechChallenge.Test.ContactTests;
 public class ContactTest
 {
-    private Mock<IContactRepository> _mockRepository = new();
-
     public static IEnumerable<object[]> InvalidContacts =>
         [
             [new Contact { Name = "Pessoa1", Email = "pessoa1", PhoneNumber = 47991598535 }],
@@ -43,59 +39,5 @@ public class ContactTest
         {
             Assert.True(false);
         }
-    }
-
-    public static IEnumerable<object[]> InsertContacts =>
-        [
-            [new Contact { Name = "Pessoa1", Email = "pessoa1@gmail.com", PhoneNumber = 47991444009, District = "SC", Region = "Sul" }],
-            [new Contact { Name = "Pessoa2", Email = "pessoa2@gmail.com", PhoneNumber = 47991666009, District = "AP", Region = "Sudeste" }],
-            [new Contact { Name = "Pessoa3", Email = "pessoa3@gmail.com", PhoneNumber = 47321330009, District = "PO", Region = "Centro-Oeste" }]
-        ];
-    [Theory]
-    [MemberData(nameof(InsertContacts))]
-    public async void Contact_ShouldBeInserted(Contact contact)
-    {
-        _mockRepository.Setup(x => x.InsertAsync(contact, CancellationToken.None)).ReturnsAsync(true);
-
-        var inserted = await _mockRepository.Object.InsertAsync(contact);
-
-        _mockRepository.Verify(r => r.InsertAsync(contact, CancellationToken.None), Times.Once);
-        Assert.True(inserted);
-    }
-
-    public static IEnumerable<object[]> UpdateContacts =>
-        [
-            [new Contact { Id = Guid.NewGuid(), Name = "Pessoa1", Email = "pessoa1@gmail.com", PhoneNumber = 47991444009, District = "SC", Region = "Sul" }],
-            [new Contact { Id = Guid.NewGuid(), Name = "Pessoa2", Email = "pessoa2@gmail.com", PhoneNumber = 47991666009, District = "AP", Region = "Sudeste" }],
-            [new Contact { Id = Guid.NewGuid(), Name = "Pessoa3", Email = "pessoa3@gmail.com", PhoneNumber = 47321330009, District = "PO", Region = "Centro-Oeste" }]
-        ];
-    [Theory]
-    [MemberData(nameof(UpdateContacts))]
-    public async void Contact_ShouldBeUpdated(Contact contact)
-    {
-        _mockRepository.Setup(x => x.UpdateAsync(contact, CancellationToken.None)).ReturnsAsync(true);
-
-        var response = await _mockRepository.Object.UpdateAsync(contact);
-
-        _mockRepository.Verify(r => r.UpdateAsync(contact, CancellationToken.None), Times.Once);
-        Assert.True(response);
-    }
-
-    public static IEnumerable<object[]> DeleteContacts =>
-        [
-            [ Guid.NewGuid() ],
-            [ Guid.NewGuid() ],
-            [ Guid.NewGuid() ]
-        ];
-    [Theory]
-    [MemberData(nameof(DeleteContacts))]
-    public async void Contact_ShouldBeDeleted(Guid id)
-    {
-        _mockRepository.Setup(x => x.DeleteAsync(id, CancellationToken.None)).ReturnsAsync(true);
-
-        var response = await _mockRepository.Object.DeleteAsync(id);
-
-        _mockRepository.Verify(x => x.DeleteAsync(id, CancellationToken.None), Times.Once);
-        Assert.True(response);
     }
 }
